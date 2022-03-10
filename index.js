d080c70fc239143ce8671b8f8334c5e74a4d995c
szabolcs-horvath <szaby0927@gmail.com>
2022-03-10 21:51:08 +0100

    Static mockup

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const http = require('http');
-
 const express = require('express');
-const { contentType } = require('express/lib/response');
 const app = express();
 app.use('/static', express.static('static'));
 
@@ -9,7 +6,4 @@ const server = app.listen(3000, function () {
     console.log('Running on :3000');
 });
 
-app.get('/static', function (req, res, next)  {
-    res.set('Content-Type', 'text/html');
-    res.send('home.html');
-});
+app.use('/static', express.static('static'));
