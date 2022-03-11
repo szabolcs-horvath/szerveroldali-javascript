69930acc401fa307536af6def3e25f5b6a003622
szabolcs-horvath <szaby0927@gmail.com>
2022-03-11 20:12:46 +0100

    Added previously forgotten C of CRUD

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
-app.use('/static', express.static('static'));
 
 const server = app.listen(3000, function () {
     console.log('Running on :3000');
 });
 
-app.use('/static', express.static('static'));
+app.use('/', express.static('static'));
