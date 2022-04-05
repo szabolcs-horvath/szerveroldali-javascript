6f08a08bababf6be6f4f5b4c5a4fc2ff7370db13
szabolcs-horvath <szaby0927@gmail.com>
2022-04-05 20:39:13 +0200

    Added requireOption

--- dbconfig/db.js
+++ dbconfig/db.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1/KJ4YY7', { useNewUrlParser: true });
+mongoose.connect('mongodb://127.0.0.1/KJ4YY7', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 module.exports = mongoose;
