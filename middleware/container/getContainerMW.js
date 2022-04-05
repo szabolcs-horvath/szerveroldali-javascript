81ff2271721d541814d534d0e48129ac5dd19206
szabolcs-horvath <szaby0927@gmail.com>
2022-04-05 20:40:33 +0200

    Converted container MWs to MongoDB

--- middleware/container/getContainerMW.js
+++ middleware/container/getContainerMW.js
@@ -1,39 +1,19 @@
 /*
  * Betölti a megadott id-vel rendelkező containert a res.locals.container-be
 */
-module.exports = function(objRep) {
-    return (req, res, next) => {
-        //TODO
-        const container = {
-            id: 1,
-            name: "Konyhai hűtő",
-            color: "#f6b73c",
-            items: [
-                {
-                    id: 1,
-                    name: "Rama margarin",
-                    amount: "200",
-                    unit: "g",
-                    expiryDate: "2022-03-12"
-                },
-                {
-                    id: 2,
-                    name: "Majonéz",
-                    amount: "1",
-                    unit: "tubus",
-                    expiryDate: "2022-11-23"
-                },
-                {
-                    id: 3,
-                    name: "Tej",
-                    amount: "1/2",
-                    unit: "l",
-                    expiryDate: "2022-02-26"
-                }
-            ]
-        };
+const requireOption = require('../../dbconfig/requireOption');
 
-        res.locals.container = container;
-        return next();
+module.exports = function(objRep) {
+    const ContainerModel = requireOption(objRep, 'ContainerModel');
+
+    return (req, res, next) => {
+        ContainerModel.findOne({ _id: req.params.containerid }, (err, container) => {
+            if (err || !container) {
+                return next(err);
+            }
+
+            res.locals.container = container;
+            return next();
+        });
     };
 };
--- middleware/container/getContainersMW.js
+++ middleware/container/getContainersMW.js
@@ -1,69 +1,19 @@
 /*
  * Betölti a res.locals.containers-be az összes containert
 */
-module.exports = function(objRep) {
-    return (req, res, next) => {
-        //TODO
-        const containers = [
-            {
-                id: 1,
-                name: "Konyhai hűtő",
-                color: "#f6b73c",
-                items: [
-                    {
-                        id: 1,
-                        name: "Rama margarin",
-                        amount: "200",
-                        unit: "g",
-                        expiryDate: "2022-03-12"
-                    },
-                    {
-                        id: 2,
-                        name: "Majonéz",
-                        amount: "1",
-                        unit: "tubus",
-                        expiryDate: "2022-11-23"
-                    },
-                    {
-                        id: 3,
-                        name: "Tej",
-                        amount: "1/2",
-                        unit: "l",
-                        expiryDate: "2022-02-26"
-                    }
-                ]
-            },
-            {
-                id: 2,
-                name: "Szekrény",
-                color: "#e66465",
-                items: [
-                    {
-                        id: 1,
-                        name: "Rama margarin",
-                        amount: "200",
-                        unit: "g",
-                        expiryDate: "2022-03-12"
-                    },
-                    {
-                        id: 2,
-                        name: "Majonéz",
-                        amount: "1",
-                        unit: "tubus",
-                        expiryDate: "2022-11-23"
-                    },
-                    {
-                        id: 3,
-                        name: "Tej",
-                        amount: "1/2",
-                        unit: "l",
-                        expiryDate: "2022-02-26"
-                    }
-                ]
-            }
-        ];
+const requireOption = require('../../dbconfig/requireOption');
 
-        res.locals.containers = containers;
-        return next();
+module.exports = function(objRep) {
+    const ContainerModel = requireOption(objRep, 'ContainerModel');
+
+    return (req, res, next) => {
+        ContainerModel.find({}, (err, containers) => {
+            if (err) {
+                return next(err);
+            }
+
+            res.locals.containers = containers;
+            return next();
+        });
     };
 };
