622c5f345b4e6566c610bff6ee16f5aece839223
szabolcs-horvath <szaby0927@gmail.com>
2022-04-05 21:06:51 +0200

    Converted container MWs to MongoDB

--- middleware/container/saveContainerMW.js
+++ middleware/container/saveContainerMW.js
@@ -18,36 +18,6 @@ module.exports = function(objRep) {
         }
 
         if (typeof res.locals.container === 'undefined') {
-            //TODO
-            // const container = {
-            //     id: 1,
-            //     name: "Konyhai hűtő",
-            //     color: "#f6b73c",
-            //     items: [
-            //         {
-            //             id: 1,
-            //             name: "Rama margarin",
-            //             amount: "200",
-            //             unit: "g",
-            //             expiryDate: "2022-03-12"
-            //         },
-            //         {
-            //             id: 2,
-            //             name: "Majonéz",
-            //             amount: "1",
-            //             unit: "tubus",
-            //             expiryDate: "2022-11-23"
-            //         },
-            //         {
-            //             id: 3,
-            //             name: "tej",
-            //             amount: "1/2",
-            //             unit: "l",
-            //             expiryDate: "2022-02-26"
-            //         }
-            //     ]
-            // };
-    
             res.locals.container = new ContainerModel();
         }
 
