bb1ead2d5bfeea591b968aa75c396c603dcfdc79
szabolcs-horvath <szaby0927@gmail.com>
2022-04-07 14:20:32 +0200

    Items are also removed when their container is removed

--- middleware/container/deleteContainerMW.js
+++ middleware/container/deleteContainerMW.js
@@ -1,5 +1,5 @@
 /*
- * Törli a res.locals.container-ben lévő containert
+ * Törli a res.locals.container-ben lévő containert és a hozzá tartozó itemeket, amik a res.locals.items-ben vannak
 */
 module.exports = function(objRep) {
     return async (req, res, next) => {
